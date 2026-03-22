8eb1e7159c5c6b72065195f0f128a52b9fa3a8fe
Hussein Darman <husseinmdarman@gmail.com>
2026-03-22 19:53:04 +0000

    added schema for use with yellow trip ddata definition

---
+++ nyc-taxi-lakehouse/utils/schema_definitions.py
@@ -0,0 +1,27 @@
+from pyspark.types import StructType, StructField, StringType, IntegerType, DoubleType, TimestampType, LongType
+
+# Define the schema for the NYC taxi trips data
+def get_nyx_taxi_trip_schema() -> StructType:
+    """Returns the schema for the NYC taxi trips data."""
+    return StructType([
+    StructField("VendorID", IntegerType(), True),
+    StructField("tpep_pickup_datetime", TimestampType(), True),
+    StructField("tpep_dropoff_datetime", TimestampType(), True),
+    StructField("passenger_count", LongType(), True),
+    StructField("trip_distance", DoubleType(), True),
+    StructField("RatecodeID", LongType(), True),
+    StructField("store_and_fwd_flag", StringType(), True),
+    StructField("PULocationID", IntegerType(), True),
+    StructField("DOLocationID", IntegerType(), True),
+    StructField("payment_type", LongType(), True),
+    StructField("fare_amount", DoubleType(), True),
+    StructField("extra", DoubleType(), True),
+    StructField("mta_tax", DoubleType(), True),
+    StructField("tip_amount", DoubleType(), True),
+    StructField("tolls_amount", DoubleType(), True),
+    StructField("improvement_surcharge", DoubleType(), True),
+    StructField("total_amount", DoubleType(), True),
+    StructField("congestion_surcharge", DoubleType(), True),
+    StructField("Airport_fee", DoubleType(), True),
+    StructField("cbd_congestion_fee", DoubleType(), True)
+])
